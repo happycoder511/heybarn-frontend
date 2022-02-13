a4c744f874c00107f3ab6e0c640dfabfaf98c1a3
happycoder511 <gnidenkobogdan67@gmail.com>
2022-02-13 11:48:18 +1000

    UPDATE TESTING AND EMAIL LOGO

--- setupTests.js
+++ setupTests.js
@@ -1,11 +1,7 @@
-if (typeof window !== "undefined") {
-  window.matchMedia =
-    window.matchMedia ||
-    function() {
-      return {
-        matches: false,
-        addListener: function() {},
-        removeListener: function() {},
-      };
-    };
-}
+window.matchMedia = window.matchMedia || function() {
+  return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {}
+  };
+};
