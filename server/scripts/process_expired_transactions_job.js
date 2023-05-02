6a02995650303a944c92734c1dff273251249015
happycoder511 <gnidenkobogdan67@gmail.com>
2023-05-03 01:15:39 +0300

    [HNZRB-23] Make Connection: non-refundable Connection Guarantee - Make job work on heroku

--- server/scripts/process_expired_transactions_job.js
+++ server/scripts/process_expired_transactions_job.js
@@ -1,11 +1,13 @@
 const { fetchAndUpdateExpiredTransactions } = require('../cancel_expired_transactions');
-const cron = require('node-cron');
+// const cron = require('node-cron');
 
 // Schedule the cron job to run every day at 00:00 (midnight)
 
-const cronTab = process.env.NODE_ENV === 'production' ? '0 0 * * *' : '*/5 * * * *';
+// const cronTab = process.env.NODE_ENV === 'production' ? '0 0 * * *' : '*/5 * * * *';
 
-cron.schedule(cronTab, async () => {
-  console.log('Running fetchAndUpdateExpiredTransactions at', new Date().toISOString());
-  await fetchAndUpdateExpiredTransactions();
-});
+// cron.schedule(cronTab, async () => {
+//   console.log('Running fetchAndUpdateExpiredTransactions at', new Date().toISOString());
+//   await fetchAndUpdateExpiredTransactions();
+// });
+
+fetchAndUpdateExpiredTransactions();
